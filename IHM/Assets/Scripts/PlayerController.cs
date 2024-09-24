009dd0a6df1981109872d660d82411ff90c84ee6
Ludo <ludovhu@gmail.com>
2024-09-24 17:40:44 +0200

    fine tuning jump in player script

--- IHM/Assets/Scripts/PlayerController.cs
+++ IHM/Assets/Scripts/PlayerController.cs
@@ -11,25 +11,35 @@ public class PlayerController : MonoBehaviour
     public Transform groundCheck;
     public LayerMask groundLayer;
     public float groundCheckRadius;
+    public int maxJumpAmount;
 
     private Gamepad manette;
 
+    private float initJumpTimer = 0.05f;
+    private bool previousGroundState;
     private bool isGrounded;
     private Rigidbody2D player;
     private Vector2 direction;
-
+    private int jumpCounter;
 
     void Start()
     {
         player = GetComponent<Rigidbody2D>();
         manette = Gamepad.current;
-
+        //jumpCounter = maxJumpAmount;
     }
 
     // Update is called once per frame
     void Update()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if (isGrounded)
+        {
+            jumpCounter = maxJumpAmount;
+        }
+
+        //previousGroundState = isGrounded;
 
         float move = Input.GetAxis("Horizontal") * moveSpeed;
 
@@ -49,11 +59,14 @@ public class PlayerController : MonoBehaviour
             player.velocity = new Vector2(move, player.velocity.y);
         }
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetButtonDown("Jump") && (jumpCounter>0))
         {
             player.velocity = new Vector2(player.velocity.x, jumpSpeed);
+            jumpCounter--;
+
         }
 
+        
 
 
     }
