50242d6f8e94e8ff7f48296aa0069328074968ac
Ludo <ludovhu@gmail.com>
2024-09-26 12:40:43 +0200

    in the process of implementing climbing

--- IHM/Assets/Scripts/PlayerController.cs
+++ IHM/Assets/Scripts/PlayerController.cs
@@ -8,18 +8,27 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     public float moveSpeed;
     public float jumpSpeed;
-    public Transform groundCheck;
+    public float climbSpeed;
+    public Transform playerLowerLeftCornerCheck;
+    public Transform playerLowerRightCornerCheck;
+    public Transform playerUpperLeftCornerCheck;
+    public Transform playerUpperRightCornerCheck;
     public LayerMask groundLayer;
+    public LayerMask climbLayer;
     public float groundCheckRadius;
+    public float climbCheckRadius;
     public int maxJumpAmount;
     public float sprintSpeedCoef;
     public float doubleTapTimeThreshold;
+    public float normalGravityScale;   
+    
 
     private Gamepad manette;
 
     private float tempStopGroundCheckTimer = 0.05f;
     private float jumpTimestamp = 0f;
     private int jumpCounter;
+    private float noGravityScale = 0f;
 
     private float leftLastTapTime = 0f;
     private float rightLastTapTime = 0f;
@@ -27,6 +36,8 @@ public class PlayerController : MonoBehaviour
 
     private bool previousGroundState;
     private bool isGrounded;
+
+    private bool isClimbing;
 
     private Rigidbody2D player;
     private Vector2 direction;
@@ -46,7 +57,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        if ((Physics2D.OverlapCircle(playerLowerLeftCornerCheck.position, groundCheckRadius, groundLayer)) || (Physics2D.OverlapCircle(playerLowerRightCornerCheck.position, groundCheckRadius, groundLayer)))
+            isGrounded = true;
+        else
+            isGrounded = false;
 
         if ((isGrounded)&&(Time.time > jumpTimestamp + tempStopGroundCheckTimer))
         {
@@ -55,9 +69,6 @@ public class PlayerController : MonoBehaviour
 
 
         float move = Input.GetAxis("Horizontal") * moveSpeed;
-
-
-
         if ((move == 0)&&(manette != null))
         {
             direction = manette.dpad.ReadValue();
@@ -67,9 +78,7 @@ public class PlayerController : MonoBehaviour
             move = direction.x * moveSpeed;
 
         }
-
         move = CheckAndApplyPlayerHorizontalSprint(move);
-
         player.velocity = new Vector2(move, player.velocity.y);
         
 
@@ -87,6 +96,12 @@ public class PlayerController : MonoBehaviour
 
     }
 
+    /* -------------------------------------------------- END OF UPDATE METHOD -------------------------------------------------- */
+
+
+    /// <summary>
+    /// Make player jump up
+    /// </summary>
     private void PlayerJumpUp()
     {
         player.velocity = new Vector2(player.velocity.x, jumpSpeed);
@@ -94,6 +109,11 @@ public class PlayerController : MonoBehaviour
         jumpTimestamp = Time.time;
     }
 
+    /// <summary>
+    /// Check if player is sprinting and if so, multiply their move speed by sprinting factor
+    /// </summary>
+    /// <param name="move">move is a float corresponding to the x-axis velovity for the player</param>
+    /// <returns></returns>
     private float CheckAndApplyPlayerHorizontalSprint(float move)
     {
         if (manette != null)
@@ -130,6 +150,70 @@ public class PlayerController : MonoBehaviour
             move *= sprintSpeedCoef;
 
         return move;
+    }
+
+    /*
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Climbable")
+        {
+            jumpCounter = maxJumpAmount;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Climbable")
+        {
+            player.gravityScale = normalGravityScale;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Climbable")
+        {
+            if ((Input.GetKeyUp(KeyCode.LeftShift)) || (Input.GetKeyUp(KeyCode.RightShift)))
+            {
+                player.gravityScale = normalGravityScale;
+                Debug.Log("shift released");
+            }
+
+            else if ((Input.GetKey(KeyCode.LeftShift)) || (Input.GetKey(KeyCode.RightShift)))
+            {
+                player.gravityScale = noGravityScale;
+
+                float move = Input.GetAxis("Vertical") * climbSpeed;
+
+                if ((move == 0) && (manette != null))
+                {
+                    direction = manette.dpad.ReadValue();
+                    if (direction == Vector2.zero)
+                        direction = manette.leftStick.ReadValue();
+
+                    move = direction.y * climbSpeed;
+                }
+
+                player.velocity = new Vector2(player.velocity.x, move);
+
+            }
+
+
+        }
+    }*/
+
+    /// <summary>
+    /// Check if player is near climb-able objects
+    /// </summary>
+    private bool CheckAndReturnIfPlayerCanClimb()
+    {
+        if ((Physics2D.OverlapCircle(playerLowerLeftCornerCheck.position, climbCheckRadius, climbLayer)) || 
+            (Physics2D.OverlapCircle(playerLowerRightCornerCheck.position, climbCheckRadius, climbLayer)) ||
+            (Physics2D.OverlapCircle(playerUpperLeftCornerCheck.position, climbCheckRadius, climbLayer)) ||
+            (Physics2D.OverlapCircle(playerUpperRightCornerCheck.position, climbCheckRadius, climbLayer)))
+            return true;
+        else
+            return false;
     }
 
     private void OnDeviceChange(InputDevice device, InputDeviceChange change)
