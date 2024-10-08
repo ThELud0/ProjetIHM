6f0e0635119a81afb455e3a6e9ef2cb16cf11c4f
Ludo <ludovhu@gmail.com>
2024-10-08 15:17:46 +0200

    shaking feedback on crumbling platforms

--- IHM/Assets/Scripts/Platform/DestructiblePlatform.cs
+++ IHM/Assets/Scripts/Platform/DestructiblePlatform.cs
@@ -1,10 +1,19 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class DestructiblePlatform : MonoBehaviour
 {
     public float destructionTime = 1f;
     public float repairTime = 1.5f;
     private GameObject platformPrefab;
+
+    public float vibrationDuration = 0.1f;   // Duration of vibration in seconds
+    public float lowFrequency = 0.2f;        // Intensity for low-frequency motor (0 to 1)
+    public float highFrequency = 0.2f;       // Intensity for high-frequency motor (0 to 1)
+
+    private bool onPlatform = false;
+
+    private Gamepad gamepad;
 
     private void Start()
     {
@@ -19,8 +28,25 @@ public class DestructiblePlatform : MonoBehaviour
         }
     }
 
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        onPlatform = true;
+        Vibrate();
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        StopVibration();
+        onPlatform = false;
+    }
+
     private void DestroyPlatform()
     {
+        if (onPlatform)
+        {
+            StopVibration();
+            onPlatform = false;
+        }
+
         gameObject.SetActive(false);
         Invoke("RepairPlatform", repairTime);
     }
@@ -29,4 +55,27 @@ public class DestructiblePlatform : MonoBehaviour
     {
         gameObject.SetActive(true);
     }
+
+    // Call this function to give vibration feedback
+    public void Vibrate()
+    {
+        gamepad = Gamepad.current;
+        if (gamepad != null)
+        {
+            // Set gamepad vibration for both low and high-frequency motors
+            gamepad.SetMotorSpeeds(lowFrequency, highFrequency);
+
+        }
+    }
+
+    // Stop the vibration
+    private void StopVibration()
+    {
+        if (gamepad != null)
+        {
+            gamepad.SetMotorSpeeds(0, 0); // Stop vibration
+        }
+    }
+
+
 }
