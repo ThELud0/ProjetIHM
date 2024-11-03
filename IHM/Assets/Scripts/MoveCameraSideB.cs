69c2a93010e0db5bc87ea8fc8e85ed2bb55230b7
Ludo <ludovhu@gmail.com>
2024-11-03 19:14:05 +0100

    fixed camera movement bug

--- IHM/Assets/Scripts/MoveCameraSideB.cs
+++ IHM/Assets/Scripts/MoveCameraSideB.cs
@@ -6,10 +6,18 @@ public class MoveCameraSideB : MonoBehaviour
     public Transform targetCameraPos;
     public Transform previousCameraPos;
     public float cameraMoveSpeed = 4f;
+    private Vector2 triggerPos;
 
     private Transform currentTarget;
     private bool isMoving = false;
 
+    private Vector2 enterPosition;
+    private Vector2 exitPosition;
+
+    private void Start()
+    {
+        triggerPos = GetComponent<Transform>().position;
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
@@ -17,8 +25,24 @@ public class MoveCameraSideB : MonoBehaviour
             float distanceToPrevious = Vector3.Distance(Camera.main.transform.position, previousCameraPos.position);
             float distanceToTarget = Vector3.Distance(Camera.main.transform.position, targetCameraPos.position);
 
+            enterPosition = collision.transform.position;
+
             currentTarget = distanceToPrevious < distanceToTarget ? targetCameraPos : previousCameraPos;
             isMoving = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            exitPosition = collision.transform.position;
+
+
+            bool samesign = (triggerPos.x - enterPosition.x < 0) == (triggerPos.x - exitPosition.x < 0);
+            if (samesign)
+                currentTarget = currentTarget == targetCameraPos? previousCameraPos : targetCameraPos;
+
         }
     }
 
